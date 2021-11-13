c18e4efdbf3c0db7411e2f5228b4ccfa6f03d9b1
kai.zhao <kiah2008@qq.com>
2021-11-13 16:33:15 +0800

    fix app crash

--- libuvccamera/src/main/jni/libusb/libusb/os/android_usbfs.c
+++ libuvccamera/src/main/jni/libusb/libusb/os/android_usbfs.c
@@ -2726,6 +2726,10 @@ static int handle_iso_completion(struct libusb_device_handle *handle,	// XXX add
 
 	usbi_mutex_lock(&itransfer->lock);
 	for (i = 0; i < num_urbs; i++) {
+		//fix app crash when plug out usb
+		if(tpriv->iso_urbs == NULL) {
+			break;
+		}
 		if (urb == tpriv->iso_urbs[i]) {
 			urb_idx = i + 1;
 			break;
